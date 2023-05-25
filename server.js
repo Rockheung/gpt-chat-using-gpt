1833352a7dfcbffc6592c6256661fad01c3e1f7f
heungjun.park <rockheung@gmail.com>
2023-05-25 18:08:24 +0900

    add server code

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/chat', async (req, res) => {
+    const prompt = req.body.prompt;
+
+    try {
+        const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+            prompt: prompt,
+            max_tokens: 150
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        res.json(response.data.choices[0].text.trim());
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
