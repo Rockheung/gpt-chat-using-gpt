cbee4b8babce5832f9083e8eb74d3b2af827a1c0
heungjun.park <rockheung@gmail.com>
2023-05-25 18:18:38 +0900

    endpoint outdate & update it from openapi docs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post('/api/chat', async (req, res) => {
     const prompt = req.body.prompt;
 
     try {
-        const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
             prompt: prompt,
             max_tokens: 150
         }, {
